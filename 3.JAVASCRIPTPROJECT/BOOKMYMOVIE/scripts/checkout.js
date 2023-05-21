be5c6a3f0e74a64d2fb5d309e1f7173b7ea95b5e
AshutoshvChavan <121417150+AshutoshvChavan1@users.noreply.github.com>
2023-05-21 12:07:12 +0530

    FristWork

---
+++ 3.JAVASCRIPTPROJECT/BOOKMYMOVIE/scripts/checkout.js
@@ -0,0 +1,29 @@
+
+let amount = localStorage.getItem("amount");
+document.getElementById("wallet").innerText=amount;
+
+let booked_movies=localStorage.getItem("movies");
+let box=document.createElement("div");
+let image=document.createElement("img");
+let title=document.createElement("h2");
+ image.src=booked_movies;
+
+ box.append(title,image);
+ document.getElementById("movie").append(box);
+
+ let confirm =()=>
+ {
+     let name=document.getElementById("user_name").value;
+     let seats=document.getElementById("number_of_seats").value;
+     let price=Number(seats)*100;
+     let balance=localStorage.getItem("amount");
+     if(price<=Number(balance))
+     {
+         alert("Booking successful!");
+         localStorage.setItem("amount",balance-price);
+         window.location.reload();
+     }
+     else{
+         alert("Insufficient Balance!");
+     }
+ }
---
+++ 3.JAVASCRIPTPROJECT/BOOKMYMOVIE/scripts/index.js
@@ -0,0 +1,13 @@
+
+let addmoney = ()=>
+{
+    let money=Number(localStorage.getItem("amount"))+Number(document.getElementById("amount").value);
+    localStorage.setItem("amount",money)
+    window.location.reload();
+}
+let money1=Number(localStorage.getItem("amount"));
+document.getElementById("wallet").innerText=money1;
+let bookmovies=()=>
+{
+    window.location.href="movies.html";
+}
